c95c78a4d003e907a57392662255902e3dab6035
Lon60 <37051290+Lon60@users.noreply.github.com>
2025-10-06 18:10:26 +0200

    setup working dev DB setup with docker-compose

--- shipkit-core/shipkit-app/build.gradle.kts
+++ shipkit-core/shipkit-app/build.gradle.kts
@@ -5,5 +5,6 @@ plugins {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql")
     implementation(project(":shipkit-api"))
 }
